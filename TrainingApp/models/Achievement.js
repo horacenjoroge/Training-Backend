3cb82ffe9aa23703346fcc0376215ae6e7466fc0
“horacenjoroge” <horacenjorge@gmail.com>
2025-05-14 23:51:30 +0300

    add achievement  and follower models

---
+++ TrainingApp/models/Achievement.js
@@ -0,0 +1,25 @@
+// models/Achievement.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const achievementSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  emoji: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Achievement', achievementSchema);
